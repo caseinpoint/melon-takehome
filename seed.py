19c979e9f22d3843c698dcc4bec21c05ca27f83e
caseinpoint <drues.other.email@gmail.com>
2022-06-17 07:37:30 -0700

    refactor: latest pushes don't satisfy requirement of 1 Reservation per time slot

--- seed.py
+++ seed.py
@@ -9,21 +9,17 @@ run(['createdb', 'melon_reservations'])
 connect_to_db(app)
 db.create_all()
 
-# generate reservations for users 0 - 9 (user0, user1, user2 ... user9)
+# generate reservations for user0
 today = date.today()
-usernames = []
 start_times = []
-
 for i in range(10):
-    usernames.append("user" + str(i))
-
     start_times.append(datetime(today.year, today.month, today.day + i, 12))
 
-for username in usernames:
-    for start_time in start_times:
-        new_reservaton = Reservation(username=username, start_time=start_time)
+username = 'user0'
+for start_time in start_times:
+    new_reservaton = Reservation(username=username, start_time=start_time)
 
-        db.session.add(new_reservaton)
+    db.session.add(new_reservaton)
 
 db.session.commit()
 
