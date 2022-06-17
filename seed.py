68cfdc199fbaf2a18a435967d4e3c5f3af64514d
caseinpoint <drues.other.email@gmail.com>
2022-06-17 07:09:52 -0700

    more specific imports

--- seed.py
+++ seed.py
@@ -1,20 +1,23 @@
 from subprocess import run
-import model
-import server
-import datetime
+from server import app
+from model import connect_to_db, db, Reservation
+from datetime import date, datetime, timedelta
 
 run(['dropdb', 'melon_reservations'])
 run(['createdb', 'melon_reservations'])
 
-model.connect_to_db(server.app)
-model.db.create_all()
+connect_to_db(app)
+db.create_all()
 
 # generate reservations for users 0 - 9 (user0, user1, user2 ... user9)
-# on dates corresponding to their user number (October 1 - 10) at 9am
+now = datetime.now()
 for i in range(10):
     username = "user" + str(i)
+
     reservation_start = datetime.datetime(2022, 5, i + 1, 9)
-    new_reservaton = model.Reservation(username=username, start_time=reservation_start)
-    model.db.session.add(new_reservaton)
-    model.db.session.commit()
+
+    new_reservaton = Reservation(username=username, start_time=reservation_start)
+
+    db.session.add(new_reservaton)
+    db.session.commit()
 
