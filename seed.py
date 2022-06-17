88d10a40378f7e9e9e1984d6e2681d82b15d0d47
caseinpoint <drues.other.email@gmail.com>
2022-06-17 07:30:32 -0700

    add more Reservations for each user

--- seed.py
+++ seed.py
@@ -10,14 +10,20 @@ connect_to_db(app)
 db.create_all()
 
 # generate reservations for users 0 - 9 (user0, user1, user2 ... user9)
-now = datetime.now()
+today = date.today()
+usernames = []
+start_times = []
+
 for i in range(10):
-    username = "user" + str(i)
+    usernames.append("user" + str(i))
 
-    reservation_start = datetime.datetime(2022, 5, i + 1, 9)
+    start_times.append(datetime(today.year, today.month, today.day + i, 12))
 
-    new_reservaton = Reservation(username=username, start_time=reservation_start)
+for username in usernames:
+    for start_time in start_times:
+        new_reservaton = Reservation(username=username, start_time=start_time)
 
     db.session.add(new_reservaton)
-    db.session.commit()
+
+db.session.commit()
 
